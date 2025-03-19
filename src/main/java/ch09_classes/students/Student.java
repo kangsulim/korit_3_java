9011cc77fc073c76abd9ef2db2525d7bf6048429
kangsulim <rkdtnfla1@naver.com>
2025-03-19 11:05:27 +0900

    feat : 20250319 student task

--- src/main/java/ch09_classes/students/Student.java
+++ src/main/java/ch09_classes/students/Student.java
@@ -1,4 +1,85 @@
 package ch09_classes.students;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+/*
+    1. 필드 정의
+        int studentCode
+        String name
+        double score
+
+    2. 기본 생성자 정의, 객체 생성 시
+        "기본 생성자로 객체가 생성되었습니다." 출력
+
+    3. studentCode 매개변수
+
+    4. name 매개변수
+
+    5. studentCode, name 매개변수
+
+    6. studentCode name socre 매개변수
+
+    7. StudentMain 클래스 생성
+        1) student1 2025001 / 김일 / 4.5
+        2) student2 2025002 / 김이 / 4.5
+        3) student3 2025003 / 김삼 / 4.5
+        4) student4 2025004 / 김사 / 4.5
+        5) student5 2025005 / 김오 / 4.5
+
+ */
 public class Student {
+    Scanner scan = new Scanner(System.in);
+    int studentCode;
+    String name;
+    double score;
+
+    Student() {
+        System.out.println("기본 생성자로 객체가 생성되었습니다.");
+    }
+
+    Student(int studentCode) {
+        System.out.println("int 매개변수 생성자로 객체가 생성되었습니다.");
+        this.studentCode = studentCode;
+    }
+
+    Student(String name) {
+        System.out.println("String 매개변수 생성자로 객체가 생성되었습니다.");
+        this.name = name;
+    }
+
+    Student(int studentCode, String name) {
+        System.out.println("int, String 매개변수 생성자로 객체가 생성되었습니다.");
+        this.studentCode = studentCode;
+        this.name = name;
+    }
+
+    Student(int studentCode, String name, double score) {
+        System.out.println("int, String, double 매개변수 생성자로 객체가 생성되었습니다.");
+        this.studentCode = studentCode;
+        this.name = name;
+        this.score = score;
+    }
+
+    void showInfo() {
+        System.out.println("\n[ " + name + " ]" + " 학생의 정보입니다.");
+        System.out.println("학번 : " + studentCode);
+        System.out.println("이름 : " + name);
+        System.out.println("점수 : " + score);
+    }
+
+    void changeName() {
+        System.out.print("바꿀 이름을 입력하시오 >>> ");
+        name = scan.nextLine();
+
+        int check;
+
+        System.out.print("바뀐 정보를 확인하시겠습니까?\n");
+        System.out.println("1. 확인하기 2. 나가기");
+        check = scan.nextInt();
+
+        if (check == 1)
+            showInfo();
+    }
 }
--- src/main/java/ch09_classes/students/StudentMain.java
+++ src/main/java/ch09_classes/students/StudentMain.java
@@ -1,4 +1,56 @@
 package ch09_classes.students;
 
 public class StudentMain {
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+        student1.studentCode = 2025001;
+        student1.name = "김일";
+        student1.score = 4.5;
+
+        Student student2 = new Student(2025002);
+        student2.name = "김이";
+        student2.score = 100;
+
+        Student student3 = new Student("김삼");
+        student3.studentCode = 2025003;
+        student3.score = 95.8;
+
+        Student student4 = new Student(2025004, "김사");
+        student4.score = 4.0;
+
+        Student student5 = new Student(2025005, "김오", 80.7);
+
+        student1.showInfo();
+        student2.showInfo();
+        student3.showInfo();
+        student4.showInfo();
+        student5.showInfo();
+
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+        Student2 student6 = new Student2();
+        student6.studentCode = 2025001;
+        student6.name = "김일";
+        student6.score = 4.5;
+
+        Student2 student7 = new Student2(2025002);
+        student7.name = "김이";
+        student7.score = 100;
+
+        Student2 student8 = new Student2("김삼");
+        student8.studentCode = 2025003;
+        student8.score = 95.8;
+
+        Student2 student9 = new Student2(2025004, "김사");
+        student9.score = 4.0;
+
+        Student2 student10= new Student2(2025005, "김오", 80.7);
+
+        student6.showInfo();
+        student7.showInfo();
+        student8.showInfo();
+        student9.showInfo();
+        student10.showInfo();
+    }
 }
